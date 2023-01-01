b67b642293c68eeea77388f3ab4c5be774157040
Kubanin <kubanin@yahoo.com>
2023-01-01 15:42:16 -0600

    secondLineAdded

--- src/test/java/interview/GitCheckOutNewBranch.java
+++ src/test/java/interview/GitCheckOutNewBranch.java
@@ -5,6 +5,7 @@ public class GitCheckOutNewBranch {
     public static void main(String[] args) {
 
         System.out.println("This message confirms that I created a new branch and now I will merge it to main branch");
+        System.out.println("Now adding another new line of code and changes");
 
 
     }
