d66c13baf91b5be53191c59f4cf0bf3157d5df92
Kubanin <kubanin@yahoo.com>
2023-01-01 15:28:12 -0600

    NewChangesOnNewBranch

---
+++ src/test/java/interview/GitCheckOutNewBranch.java
@@ -0,0 +1,12 @@
+package interview;
+
+public class GitCheckOutNewBranch {
+
+    public static void main(String[] args) {
+
+        System.out.println("This message confirms that I created a new branch and now I will merge it to main branch");
+
+
+    }
+
+}
